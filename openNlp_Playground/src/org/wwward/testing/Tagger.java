36cd8d4d5c384118ace6e5045e92270fb697d11d
William Ward <wwward@pobox.com>
2012-04-25 16:50:31 -0400

    packing up, not complete.

--- openNlp_Playground/src/org/wwward/testing/Tagger.java
+++ openNlp_Playground/src/org/wwward/testing/Tagger.java
@@ -3,6 +3,8 @@ package org.wwward.testing;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import opennlp.maxent.BasicContextGenerator;
 import opennlp.maxent.ContextGenerator;
@@ -20,17 +22,18 @@ public class Tagger {
 		model = m;
 	}
 	
-	private void eval (String predicates) {
-		eval(predicates, false);
-	}
-	
-	private void eval (String predicates, boolean real) {
+	private HashMap<String, Double> eval (String predicates, boolean real) {
 		String[] contexts = predicates.split(" ");
 		double[] ocs;
+		HashMap<String, Double> outcomes = new HashMap<String, Double>();
 		ocs = model.eval(contexts);
 		System.out.println("For context: " + predicates + "\n" + model.getAllOutcomes(ocs) + "\n");
+//		System.out.println("Best: " + model.getBestOutcome(ocs) + "\n");
+		for (int i = 0; (i < ocs.length); i++) {
+			outcomes.put(model.getOutcome(i), ocs[i]);
+		}
+		return outcomes;
 	}
-	
 	
 	
 	/**
@@ -53,15 +56,39 @@ public class Tagger {
 		
 		try {
 			DataStream ds = new PlainTextByLineDataStream(new FileReader(new File(dataFileName)));
-			
+			ArrayList<String> obs = new ArrayList<String>();
+			HashMap<String, Double> maxobs = new HashMap<String, Double>();
 			while (ds.hasNext()) {
 				String s = (String)ds.nextToken();
-				System.out.println(s);
-				//String sub = s.substring(0, s.lastIndexOf('\t'));
-				String[] split = s.split("\t");
-				String sub = split[0];
-				System.out.println(sub); //debug
-				predictor.eval(sub);
+				obs.add(s);
+				if (s.trim().isEmpty()) {
+					HashMap<String, HashMap<String, Double>> V = new HashMap<String, HashMap<String, Double>>();
+//					for (String observation : obs) {
+//						double[] ocsresult = predictor.eval(observation, false);
+//						
+//					}
+					for (String observation : obs) {
+						if (!observation.trim().isEmpty()) {
+							String[] temp = observation.split("\t");
+							String substring = temp[0];
+							V.put(observation, predictor.eval(substring, false));
+						}
+					}
+					for (String observation : obs) {
+						if(!observation.trim().isEmpty()) {
+							HashMap<String, Double> tempmap = V.get(observation);
+							HashMap<String, Double> hashmap = 
+						}
+					}
+					
+					obs.clear(); //reset for next loop
+					
+				//} else {
+				//String sub = s.substring(0, s.lastIndexOf('\t')); //not used calling viterbi
+				//predictor.eval(sub); use this from within viterbi
+				//add to sentence
+				}
+				//call to viterbi unless empty, then quit
 			}
 			return;
 			
@@ -69,7 +96,5 @@ public class Tagger {
 			System.err.println("main: ERROR opening dataFileName");
 			e.printStackTrace();
 		}
-
 	}
-
 }
