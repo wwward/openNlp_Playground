ff325ce6b2ee2a4f13315f83598dcf3a9180cc1e
William Ward <wwward@pobox.com>
2012-04-25 19:55:40 -0400

    working but poor accuracy, fix viterbi implementation - due saturday

--- openNlp_Playground/src/org/wwward/testing/Tagger.java
+++ openNlp_Playground/src/org/wwward/testing/Tagger.java
@@ -3,8 +3,12 @@ package org.wwward.testing;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 import opennlp.maxent.BasicContextGenerator;
 import opennlp.maxent.ContextGenerator;
@@ -40,9 +44,18 @@ public class Tagger {
 	 * @param args datafilename modelfilename
 	 */
 	public static void main(String[] args) {
-		String dataFileName, modelFileName;
+		String dataFileName, modelFileName, outFileName;
 		dataFileName = args[0];
 		modelFileName = args[1];
+		outFileName = args[2]; // output file with added tag
+		
+		FileWriter outFile = null;
+		try {
+			outFile = new FileWriter(new File(outFileName)); // open file for write
+		} catch (Exception e) {
+			System.err.println("main: ERROR unable to open output file for write.");
+			e.printStackTrace();
+		}
 		
 		Tagger predictor = null;
 		
@@ -62,7 +75,9 @@ public class Tagger {
 				String s = (String)ds.nextToken();
 				obs.add(s);
 				if (s.trim().isEmpty()) {
-					HashMap<String, HashMap<String, Double>> V = new HashMap<String, HashMap<String, Double>>();
+					HashMap<String, HashMap<String, Double>> V = new HashMap<String, HashMap<String, Double>>(); // regular Viterbi table of observations + probabilities
+					HashMap<String, HashMap<String, Double>> maxValues = new HashMap<String, HashMap<String, Double>>(); //store highest valued B/I/O results per word
+					LinkedList<String> path = new LinkedList<String>(); // tags will be added here and read out to a file later.
 //					for (String observation : obs) {
 //						double[] ocsresult = predictor.eval(observation, false);
 //						
@@ -77,10 +92,38 @@ public class Tagger {
 					for (String observation : obs) {
 						if(!observation.trim().isEmpty()) {
 							HashMap<String, Double> tempmap = V.get(observation);
-							HashMap<String, Double> hashmap = 
+							Map.Entry<String, Double> maxEntry = null;
+							for (Map.Entry<String, Double> entry : tempmap.entrySet()) {
+							    if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+							        maxEntry = entry;
+							    }
+							}
+							String tag = maxEntry.getKey();
+							path.add(tag); // put the maximum scored tag in here - refactor this code
+							Double tval = maxEntry.getValue(); //store max probability
+							HashMap<String, Double> tempentry = new HashMap<String, Double>();
+							tempentry.put(tag,tval); //just used to manipulate the loose key and value into a compatible type
+							maxValues.put(observation, tempentry); // store maximum value for this observation
 						}
 					}
-					
+					System.out.println(obs);
+					System.out.println(path);
+					for (String observation : obs) {
+						try {
+							if (!observation.trim().isEmpty()) {
+								String[] obsplit = observation.split("\t");
+								String obsspace = obsplit[0] + " " + obsplit[1] + " " + obsplit[2]; //crude way of changing tabs to spaces for conlleval
+								outFile.write(obsspace + " " + path.pop() + "\n");
+							} else {
+								outFile.write("\n"); // if space, just output newline
+								outFile.flush();
+							}
+						} catch (IOException e) {
+							System.err.println("main: ERROR Unable to write out in final output step.");
+							e.printStackTrace();
+						}
+					}
+					maxValues.clear(); // reset for next loop
 					obs.clear(); //reset for next loop
 					
 				//} else {
@@ -89,6 +132,12 @@ public class Tagger {
 				//add to sentence
 				}
 				//call to viterbi unless empty, then quit
+			}
+			try {
+				outFile.close();
+			} catch (IOException e) {
+				System.err.println("main: ERROR unable to close output file.");
+				e.printStackTrace();
 			}
 			return;
 			
